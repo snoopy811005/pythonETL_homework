e766e7327991f99ca0b44e37e82409e5d9807ee6
snoopy811005 <snoopy811005@gmail.com>
2021-11-16 01:32:09 +0800

    1.Modify README.md 2.Add argparse(main.py) 3.Get multiple pages' jobInfo(main.py)

--- README.md
+++ README.md
@@ -8,19 +8,36 @@
    4. json
    5. pandas
    6. xlsxwriter(for excel)
+   7. argparse
 2. HTTP Method: GET
 3. URL: https://www.104.com.tw/jobs/search/
 4. Parameter:
    1. ro=0
    2. kwop=7
-   3. keyword=<user_input>
+   3. keyword=<user_input>(with default)
    4. expansionType=area,spec,com,job,wf,wktm
    5. order=12
    6. asc=0
-   7. page=<auto_increment>
+   7. page=<user_input>(with default)
    8. mode=s
    9. jobsource=2018indexpoc
    10. langFlag=0
 5. JS render solution:
    1. url: https://www.104.com.tw/job/ajax/content/<jobID>
    2. headers: { "Referer": "https://www.104.com.tw/job/<jobID>" }
+
+---------------------------------
+
+步驟1<br>
+Input '--help' to see parameters<br>
+![img.png](img.png)
+<br>
+<br>
+步驟2<br>
+Input parameters '-k/--keyword' and '-p/--page'<br>
+![img_1.png](img_1.png)
+<br>
+<br>
+步驟3<br>
+Save to CSV/XLSX<br>
+![img_2.png](img_2.png)
--- main.py
+++ main.py
@@ -3,76 +3,92 @@ from bs4 import BeautifulSoup
 import time
 import json
 import pandas as pd
+from argparse import ArgumentParser
 
 def main():
+    #argument
+    parser = ArgumentParser()
+    parser.add_argument('-k',
+                        '--keyword',
+                        help='search keyword',
+                        type=str,
+                        default='data scientist')
+    parser.add_argument('-p',
+                        '--page',
+                        help='how many pages you want to search',
+                        type=int,
+                        default=1)
+    args =parser.parse_args()
+
     User_Agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36 Edg/95.0.1020.40"
     headers = {'User-Agent': User_Agent}
 
-    parameterDict = {'ro': '0',
-                     'kwop': '7',
-                     'keyword': 'Data%20Scientist', # '%20' = ' '
-                     'expansionType': 'area%2Cspec%2Ccom%2Cjob%2Cwf%2Cwktm', # '%2C' = ','
-                     'order': '12',
-                     'asc': '0',
-                     'page': '0',
-                     'mode': 's',
-                     'jobsource': '2018indexpoc',
-                     'langFlag': '0'}
-    parameterList = [key+'='+val for key, val in parameterDict.items()]
-
-    url = "https://www.104.com.tw/jobs/search/" + '?' + '&'.join(parameterList)
-    # print(url)
-
     ss = requests.session()
 
-    res = ss.get(url, headers=headers)
-    soup = BeautifulSoup(res.text, 'html.parser')
-    # print(soup)
-
-    # article
-    articleList = soup.select('article')
-    articleTitleUrlList = list()
-    for i in articleList:
-        try:
-            articleTitle = i.select('a[class="js-job-link"]')[0].text
-            articleUrl = "https:" + i.select('a[class="js-job-link"]')[0]['href']
-            articleTitleUrlList.append((articleTitle, articleUrl))
-            # print(articleTitle)
-            # print(articleUrl)
-        except:
-            pass
-            # print(i.select('a[class="js-job-link"]'))
-        # print('=============')
-
-    # company, job_title, job_content, job_category
     jobInfo = []
-    for articleTitle, articleUrl in articleTitleUrlList:
-        print(articleTitle)
-        print(articleUrl)
+    for pg in range(args.page):
+        parameterDict = {'ro': '0',
+                         'kwop': '7',
+                         'keyword': args.keyword, # 'Data%20Scientist', # '%20' = ' '
+                         'expansionType': 'area%2Cspec%2Ccom%2Cjob%2Cwf%2Cwktm', # '%2C' = ','
+                         'order': '12',
+                         'asc': '0',
+                         'page': str(pg), # '0',
+                         'mode': 's',
+                         'jobsource': '2018indexpoc',
+                         'langFlag': '0'}
+        parameterList = [key+'='+val for key, val in parameterDict.items()]
 
-        jobUrl = articleUrl.split('?')[0]
-        jobUrlAhead = jobUrl.split('://')[0]
-        jobUrlBehindList = jobUrl.split('://')[1].split('/')
-        jobUrlBehindList.insert(-1, 'ajax')
-        jobUrlBehindList.insert(-1, 'content')
-        jobJsUrl = jobUrlAhead + '://' + '/'.join(jobUrlBehindList)
-        print('\t' + jobUrl)
-        print('\t' + jobJsUrl)
+        url = "https://www.104.com.tw/jobs/search/" + '?' + '&'.join(parameterList)
+        # print(url)
 
-        headers['Referer'] = jobUrl
-        articleRes = ss.get(jobJsUrl, headers=headers)
-        jsonArticleRes = json.loads(articleRes.text)
-        company = jsonArticleRes['data']['header']['custName']
-        jobTitle = jsonArticleRes['data']['header']['jobName']
-        jobContent = jsonArticleRes['data']['jobDetail']['jobDescription']
-        jobCategoryList = [i['description'] for i in jsonArticleRes['data']['jobDetail']['jobCategory']]
-        jobInfo.append([jobTitle, company, ','.join(jobCategoryList), jobContent])
-        print('\t\t' + company)
-        print('\t\t' + jobTitle)
-        print('\t\t' + jobContent)
-        print('\t\t' + ','.join(jobCategoryList))
-        print('=============')
-        # time.sleep(3)
+        res = ss.get(url, headers=headers)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        # print(soup)
+
+        # article
+        articleList = soup.select('article')
+        articleTitleUrlList = list()
+        for i in articleList:
+            try:
+                articleTitle = i.select('a[class="js-job-link"]')[0].text
+                articleUrl = "https:" + i.select('a[class="js-job-link"]')[0]['href']
+                articleTitleUrlList.append((articleTitle, articleUrl))
+                # print(articleTitle)
+                # print(articleUrl)
+            except:
+                pass
+                # print(i.select('a[class="js-job-link"]'))
+            # print('=============')
+
+        # company, job_title, job_content, job_category
+        for articleTitle, articleUrl in articleTitleUrlList:
+            print(articleTitle)
+            print(articleUrl)
+
+            jobUrl = articleUrl.split('?')[0]
+            jobUrlAhead = jobUrl.split('://')[0]
+            jobUrlBehindList = jobUrl.split('://')[1].split('/')
+            jobUrlBehindList.insert(-1, 'ajax')
+            jobUrlBehindList.insert(-1, 'content')
+            jobJsUrl = jobUrlAhead + '://' + '/'.join(jobUrlBehindList)
+            print('\t' + jobUrl)
+            print('\t' + jobJsUrl)
+
+            headers['Referer'] = jobUrl
+            articleRes = ss.get(jobJsUrl, headers=headers)
+            jsonArticleRes = json.loads(articleRes.text)
+            company = jsonArticleRes['data']['header']['custName']
+            jobTitle = jsonArticleRes['data']['header']['jobName']
+            jobContent = jsonArticleRes['data']['jobDetail']['jobDescription']
+            jobCategoryList = [i['description'] for i in jsonArticleRes['data']['jobDetail']['jobCategory']]
+            jobInfo.append([jobTitle, company, ','.join(jobCategoryList), jobContent])
+            print('\t\t' + company)
+            print('\t\t' + jobTitle)
+            # print('\t\t' + jobContent)
+            print('\t\t' + ','.join(jobCategoryList))
+            print('=============')
+            time.sleep(1)
 
     # integrate job information
     columns = ['Job Opening', 'Job Company', 'Job Category', 'Job Content']
